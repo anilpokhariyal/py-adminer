8921f2a7031be36b49c5e08486a03f7df4dde6bd
Anil Pokhariyal <anil@bjshomedelivery.com>
2022-04-06 16:49:07 +0530

    docker setup fixed

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,1 +1,1 @@
-docker network create py_network || true && docker-compose -f docker/docker-compose.yml build py-adminer && docker-compose -f docker/docker-compose.yml  up -d py-adminer
+docker network create py_network || true && docker-compose -f docker/docker-compose.yml build py-adminer && docker-compose -f docker/docker-compose.yml up -d py-adminer
